7a05c0aac2ac3847ca4f5eed066d0b21ab492c1f
Balays33 <40806995+Balays33@users.noreply.github.com>
2020-03-24 21:56:24 +0000

    Update TwoSum.java

--- JavaCodingInterview/src/javacodinginterview/TwoSum.java
+++ JavaCodingInterview/src/javacodinginterview/TwoSum.java
@@ -22,16 +22,34 @@ package javacodinginterview;
  */
 public class TwoSum {
     
-    private int[] result;
+    private int[] result = new int[2];
     
     public int[] solution (int[] nums, int target) {
+        
         
         for(int i=0;i<nums.length;i++){
             System.out.println(" nums: "+nums[i]);
         }
         System.out.println("target"+target);
-      
         
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+            
+                if (nums[i]!=nums[j]){
+                if (nums[i]+nums[j]==target){
+                    result[0]=nums[i];
+                    result[1]=nums[j];
+                    break;
+                }
+                }
+            }
+        }
+      
+        if (result[0]==0){
+             System.out.println(" no result ");
+        } else{
+        System.out.println(result[0]+":"+result[1]);
+        }
         return result;
     }
     
